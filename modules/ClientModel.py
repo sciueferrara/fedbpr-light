a0598f40c784cc83d21190946447aa41425f1ced
sciueferrara <antonioferrara94@icloud.com>
2020-05-26 22:20:56 +0200

    light version

--- modules/ClientModel.py
+++ modules/ClientModel.py
@@ -8,7 +8,7 @@ class ClientModel:
         self.user_vec = np.random.randn(n_factors) / 10
 
     def predict(self, server_model):
-        return np.dot(server_model.item_vecs, self.user_vec) + self.item_bias
+        return np.dot(server_model.item_vecs, self.user_vec) + server_model.item_bias
 
     def predict_one(self, i, server_model):
-        return np.dot(server_model.item_vecs[i], self.user_vec) + self.item_bias[i]
+        return np.dot(server_model.item_vecs[i], self.user_vec) + server_model.item_bias[i]
